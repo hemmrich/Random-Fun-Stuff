95070ee4777c16895f0f312ad405d26355aa18a5
Maximilian Hemmrich <maximilian.hemmrich@gmail.com>
2015-02-22 01:00:02 -0500

    Finished HTML to CSV script

---
+++ Python/Machine Learning Investing/ScrapedHTMLtoCSV.py
@@ -0,0 +1,42 @@
+# use pandas to output D/E data to CSV
+
+import pandas as pd
+import os
+import time
+from datetime import datetime
+
+path = "/Users/Max/SkyDrive/Programming/GitHub/intraQuarter"
+
+def Key_Stats(gather = "Total Debt/Equity (mrq)"):
+    statspath = path + '/_KeyStats'
+    stock_list = [x[0] for x in os.walk(statspath)]  # all company directories
+    df = pd.DataFrame(columns=['Date', 'Unix', 'Ticker', 'DE Ratio'])
+
+    for each_dir in stock_list[1:]:  # first elt is root directory itself
+        each_file = os.listdir(each_dir)
+
+        ticker = each_dir.split("/")[-1]
+
+        if len(each_file) > 0:
+            for file in each_file:
+                date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
+                unix_time = time.mktime(date_stamp.timetuple())
+
+                full_file_path = each_dir + '/' + file
+                source = open(full_file_path, 'r').read()
+                try:
+                    value = float(source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                    df = df.append({'Date': date_stamp, 'Unix': unix_time, 'Ticker': ticker, 'DE Ratio': value}, ignore_index=True)
+                except Exception as e:
+                    pass
+
+    save = gather.replace(' ', '').replace(')', '').replace('(', '').replace('/', '') + '.csv'
+
+    print(save)
+    df.to_csv(save)
+    print('Saved to CSV')
+
+
+
+Key_Stats()
+
